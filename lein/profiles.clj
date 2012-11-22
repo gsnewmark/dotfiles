c6abbfd908686e8d613cc22f1871e6948fd691e9
gsnewmark <gsnewmark@meta.ua>
2012-11-23 00:43:32 +0200

    Add the leiningen profile.

---
+++ lein/profiles.clj
@@ -0,0 +1,9 @@
+{:user
+  {:plugins [[lein-marginalia "0.7.1"]
+             [jonase/kibit "0.0.7"]
+             [lein-midje "2.0.1"]
+             [lein-ring "0.7.5"]
+             [lein-cljsbuild "0.2.9"]
+             [lein-outdated "1.0.0"]
+             [lein-gitify "0.1.0"]
+             [lein-pedantic "0.0.5"]]}}
