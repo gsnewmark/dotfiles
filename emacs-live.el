7615a44f8cf39d70ff6bdaf749ccc61152b21f74
Ivan Kryvoruchko <gildraug@gmail.com>
2013-09-03 20:36:13 +0300

    Fix (load missing emacs-live pack).

--- emacs-live.el
+++ emacs-live.el
@@ -1,9 +1,10 @@
 (live-use-packs '(live/foundation-pack
+                  ;live/colour-pack
                   live/clojure-pack
                   live/lang-pack
                   live/power-pack
                   live/git-pack
-                  ;live/colour-pack
+                  live/org-pack
                   live/bindings-pack))
 (live-add-packs '(~/.live-packs/gsnewmark-appearence-pack))
 (live-add-packs '(~/.live-packs/gsnewmark-foundation-pack))
