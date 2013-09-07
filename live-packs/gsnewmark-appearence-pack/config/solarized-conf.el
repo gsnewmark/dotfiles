c4473aa8ef9d3fb079e61852425fb0d4cfdefcb6
Ivan Kryvoruchko <gildraug@gmail.com>
2013-09-07 15:09:39 +0300

    Don't use default font for org-mode headings.

--- live-packs/gsnewmark-appearence-pack/config/solarized-conf.el
+++ live-packs/gsnewmark-appearence-pack/config/solarized-conf.el
@@ -1,4 +1,6 @@
 (live-add-pack-lib "solarized-emacs")
 (add-to-list 'custom-theme-load-path
              (concat (live-pack-lib-dir) "solarized-emacs"))
+(custom-set-variables
+ '(solarized-use-variable-pitch nil))
 (load-theme 'solarized-dark t)
