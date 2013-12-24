6567bdf3c82d52c6839b932f0252b118eb82d99d
Ivan Kryvoruchko <gildraug@gmail.com>
2013-12-24 22:17:42 +0200

    Update Leiningen plugins; Solarized Emacs color theme.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,5 @@
-{:user {:plugins [[lein-try "0.3.0"]
-                  [lein-ancient "0.4.4"]
+{:user {:plugins [[lein-try "0.4.1"]
+                  [lein-ancient "0.5.4"]
                   [lein-bikeshed "0.1.3"]
-                  [lein-kibit "0.0.8"]]}}
+                  [lein-kibit "0.0.8"]
+                  [jonase/eastwood "0.0.2"]]}}
