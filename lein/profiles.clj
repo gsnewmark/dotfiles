942fe4e0f5939d8dd3ac51c9bc62bd30fc969f05
Ivan Kryvoruchko <gildraug@gmail.com>
2013-12-24 22:32:37 +0200

    Add Slamhound to Clojure toolbox.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,7 @@
-{:user {:plugins [[lein-try "0.4.1"]
+{:user {:dependencies [[slamhound "1.5.0"]]
+        :plugins [[lein-try "0.4.1"]
                   [lein-ancient "0.5.4"]
                   [lein-bikeshed "0.1.3"]
                   [lein-kibit "0.0.8"]
-                  [jonase/eastwood "0.0.2"]]}}
+                  [jonase/eastwood "0.0.2"]]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
