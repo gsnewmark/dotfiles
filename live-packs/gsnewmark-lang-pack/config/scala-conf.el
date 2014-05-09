8fa52363084d69d47ced6443dc4c3bc0e0eeb970
Ivan Kryvoruchko <gildraug@gmail.com>
2014-05-09 12:23:10 +0300

    Use sbt-mode instead of ensime.

--- live-packs/gsnewmark-lang-pack/config/scala-conf.el
+++ live-packs/gsnewmark-lang-pack/config/scala-conf.el
@@ -1,21 +1,27 @@
+(live-add-pack-lib "sbt-mode")
+(require 'sbt-mode)
+
+(add-hook 'sbt-mode-hook
+          '(lambda ()
+             (setq compilation-skip-threshold 1)
+             (local-set-key (kbd "C-a") 'comint-bol)
+             (local-set-key (kbd "M-RET") 'comint-accumulate)))
+
 (live-add-pack-lib "scala-mode2")
 (require 'scala-mode2)
 
-(live-add-pack-lib "ensime/elisp")
-(require 'ensime)
-
 (custom-set-variables
- '(scala-indent:default-run-on-strategy scala-indent:reluctant-strategy))
+ '(scala-indent:default-run-on-strategy scala-indent:eager-strategy))
 
 (add-hook 'scala-mode-hook
           '(lambda ()
-             (ensime-scala-mode-hook)
              (local-set-key (kbd "RET")
                             '(lambda ()
                                (interactive)
                                (newline-and-indent)
                                (scala-indent:insert-asterisk-on-multiline-comment)))
-             (local-set-key (kbd "C-M-j") 'join-line)
              (local-set-key (kbd "<backtab>")
-                            'scala-indent:indent-with-reluctant-strategy)))
+                            'scala-indent:indent-with-reluctant-strategy)
+             (local-set-key (kbd "M-.") 'sbt-find-definitions)
+             (local-set-key (kbd "C-x '") 'sbt-run-previous-command)))
 (add-hook 'scala-mode-hook 'flyspell-prog-mode)
