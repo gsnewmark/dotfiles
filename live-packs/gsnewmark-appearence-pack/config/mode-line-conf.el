c970475a14c6dc69806457e4822addb742f2b528
Ivan Kryvoruchko <gildraug@gmail.com>
2014-06-07 16:39:21 +0300

    Switch to smart-mode-line.

---
+++ live-packs/gsnewmark-appearence-pack/config/mode-line-conf.el
@@ -0,0 +1,20 @@
+(live-add-pack-lib "smart-mode-line")
+
+(setq sml/theme 'respectful)
+(require 'smart-mode-line)
+(sml/setup)
+
+(setq sml/shorten-directory t)
+(setq sml/shorten-modes t)
+(setq sml/name-width 35)
+(setq sml/mode-width 'full)
+
+(add-to-list 'sml/hidden-modes " AC")
+(add-to-list 'sml/hidden-modes " WS")
+(add-to-list 'sml/hidden-modes " Undo-Tree")
+(add-to-list 'sml/hidden-modes " VHl")
+(add-to-list 'sml/hidden-modes " SliNav")
+(add-to-list 'sml/hidden-modes " Fill")
+(add-to-list 'sml/hidden-modes " yas")
+(add-to-list 'sml/hidden-modes " cljr")
+(add-to-list 'sml/hidden-modes " G-+")
--- live-packs/gsnewmark-appearence-pack/init.el
+++ live-packs/gsnewmark-appearence-pack/init.el
@@ -1,2 +1,3 @@
 (live-load-config-file "solarized-conf.el")
+(live-load-config-file "mode-line-conf.el")
 (live-load-config-file "misc-conf.el")
