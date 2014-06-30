3e3140fd31a0f05de598ca45875b9edefe6c0366
Ivan Kryvoruchko <gildraug@gmail.com>
2014-06-30 15:02:25 +0300

    Update common Clojure plugins, scala-mode2.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,8 @@
-{:user {:dependencies [[slamhound "1.5.2"]]
-        :plugins [[lein-try "0.4.1"]
-                  [lein-ancient "0.5.5-SNAPSHOT"]
-                  [lein-bikeshed "0.1.6"]
+{:user {:dependencies [[slamhound "1.5.5"]]
+        :plugins [[cider/cider-nrepl "0.7.0-SNAPSHOT"]
+                  [lein-try "0.4.1"]
+                  [lein-ancient "0.6.0-SNAPSHOT"]
+                  [lein-bikeshed "0.1.7"]
                   [lein-kibit "0.0.8"]
-                  [jonase/eastwood "0.1.0"]]
+                  [jonase/eastwood "0.1.4"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
