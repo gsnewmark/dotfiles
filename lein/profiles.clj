31258269c9366ecd09fa9b0157a93f57c3b9cf35
Ivan Kryvoruchko <gildraug@gmail.com>
2014-07-18 22:48:04 +0300

    Add lein-gorilla Leiningen plugin.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -4,5 +4,6 @@
                   [lein-ancient "0.6.0-SNAPSHOT"]
                   [lein-bikeshed "0.1.7"]
                   [lein-kibit "0.0.8"]
-                  [jonase/eastwood "0.1.4"]]
+                  [jonase/eastwood "0.1.4"]
+                  [lein-gorilla "0.2.0"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
