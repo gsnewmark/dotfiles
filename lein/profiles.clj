e0a2b8d7a73e39270fac125d1f4e3faf4d8f4149
Ivan Kryvoruchko <gildraug@gmail.com>
2014-08-05 22:12:32 +0300

    Update Cider middleware version.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,9 +1,9 @@
 {:user {:dependencies [[slamhound "1.5.5"]]
-        :plugins [[cider/cider-nrepl "0.7.0-SNAPSHOT"]
+        :plugins [[cider/cider-nrepl "0.7.0"]
                   [lein-try "0.4.1"]
                   [lein-ancient "0.6.0-SNAPSHOT"]
                   [lein-bikeshed "0.1.7"]
                   [lein-kibit "0.0.8"]
                   [jonase/eastwood "0.1.4"]
-                  [lein-gorilla "0.2.0"]]
+                  [lein-gorilla "0.3.1"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
