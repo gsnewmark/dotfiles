ac711642594a2176b6a74b1f4bdcb05073603bed
Ivan Kryvoruchko <gildraug@gmail.com>
2014-09-20 11:02:53 +0300

    Update Leiningen plugin version.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,7 +2,7 @@
         :plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]
                   [lein-try "0.4.3"]
                   [lein-ancient "0.6.0-SNAPSHOT"]
-                  [lein-bikeshed "0.1.7"]
+                  [lein-bikeshed "0.1.8"]
                   [lein-kibit "0.0.8"]
                   [jonase/eastwood "0.1.4"]
                   [lein-gorilla "0.3.3"]]
