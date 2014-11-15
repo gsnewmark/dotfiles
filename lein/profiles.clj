16508d18c7fd541a9adf1d463fa099441f0e1ef1
Ivan Kryvoruchko <gildraug@gmail.com>
2014-11-15 12:45:11 +0200

    Update Leiningen eastwood plugin

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -4,6 +4,6 @@
                   [lein-ancient "0.6.0-SNAPSHOT"]
                   [lein-bikeshed "0.1.8"]
                   [lein-kibit "0.0.8"]
-                  [jonase/eastwood "0.1.4"]
+                  [jonase/eastwood "0.1.5"]
                   [lein-gorilla "0.3.3"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
