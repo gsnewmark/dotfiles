f246d9ecba26ab857f818c810fd4301ea2e3fa45
Ivan Kryvoruchko <gildraug@gmail.com>
2014-11-16 20:01:15 +0200

    Workaround for Lein bug.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user {:dependencies [[slamhound "1.5.5"]]
         :plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]
                   [lein-try "0.4.3"]
-                  [lein-ancient "0.6.0-SNAPSHOT"]
+                  [lein-ancient "0.6.0-SNAPSHOT" :exclusions [org.clojure/core.cache]]
                   [lein-bikeshed "0.1.8"]
                   [lein-kibit "0.0.8"]
                   [jonase/eastwood "0.1.5"]
