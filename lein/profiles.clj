e839de1eb32510a5a7b4ac73a219473ce0666002
Ivan Kryvoruchko <gildraug@gmail.com>
2014-11-22 13:54:55 +0200

    Update Leiningen plugins

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,9 +1,9 @@
 {:user {:dependencies [[slamhound "1.5.5"]]
-        :plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]
+        :plugins [[cider/cider-nrepl "0.8.1"]
                   [lein-try "0.4.3"]
                   [lein-ancient "0.6.0-SNAPSHOT" :exclusions [org.clojure/core.cache]]
                   [lein-bikeshed "0.1.8"]
                   [lein-kibit "0.0.8"]
-                  [jonase/eastwood "0.1.5"]
+                  [jonase/eastwood "0.2.0"]
                   [lein-gorilla "0.3.3"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
