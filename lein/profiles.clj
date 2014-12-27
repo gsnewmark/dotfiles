6272c352a64211f18aad1b4ed0b8818f5385622d
Ivan Kryvoruchko <gildraug@gmail.com>
2014-12-27 13:40:03 +0200

    Update cider-nrepl

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:dependencies [[slamhound "1.5.5"]]
-        :plugins [[cider/cider-nrepl "0.8.2"]
+        :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [refactor-nrepl "0.2.2"]
                   [lein-try "0.4.3"]
                   [lein-ancient "0.6.0-SNAPSHOT"]
