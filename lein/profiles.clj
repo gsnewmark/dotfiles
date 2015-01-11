435509af6f5ba75336516c3a7e417dcfdc1eb2f8
Ivan Kryvoruchko <gildraug@gmail.com>
2015-01-11 14:14:49 +0200

    Start configuring flycheck-clojure

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,10 +1,9 @@
-{:user {:dependencies [[slamhound "1.5.5"]]
+{:user {:dependencies [[slamhound "1.5.5"]
+                       [acyclic/squiggly-clojure "0.1.2-SNAPSHOT"]]
         :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [refactor-nrepl "0.2.2"]
                   [lein-try "0.4.3"]
                   [lein-ancient "0.6.0-SNAPSHOT"]
                   [lein-bikeshed "0.2.0"]
-                  [lein-kibit "0.0.8"]
-                  [jonase/eastwood "0.2.1"]
                   [lein-gorilla "0.3.4"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
