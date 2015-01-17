9a00ac9c4460bee51860b1046bf2e0a6bbb8bd70
Ivan Kryvoruchko <gildraug@gmail.com>
2015-01-17 19:37:05 +0200

    Revert refactor-nrepl update because it's affects figwheel

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user {:dependencies [[slamhound "1.5.5"]
                        [acyclic/squiggly-clojure "0.1.2-SNAPSHOT"]]
         :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [refactor-nrepl "0.3.0-SNAPSHOT"]
+                  [refactor-nrepl "0.2.2"]
                   [lein-try "0.4.3"]
                   [lein-ancient "0.6.0-SNAPSHOT"]
                   [lein-bikeshed "0.2.0"]
