25cc1d2e5d353edf7bedd02d7078230e3e6fa033
Ivan Kryvoruchko <gildraug@gmail.com>
2015-02-21 12:22:11 +0200

    Update Emacs config

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,4 @@
-{:user {:dependencies [[slamhound "1.5.5"]
-                       [acyclic/squiggly-clojure "0.1.2-SNAPSHOT"]]
+{:user {:dependencies [[slamhound "1.5.5"]]
         :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [refactor-nrepl "0.2.2"]
                   [lein-try "0.4.3"]
