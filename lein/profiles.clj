661c918f62b8e9a449ea189511643df396d4b591
Ivan Kryvoruchko <gildraug@gmail.com>
2015-03-14 12:01:32 +0200

    Update Emacs config and nrepl version

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,5 @@
-{:user {:dependencies [[slamhound "1.5.5"]]
+{:user {:dependencies [[slamhound "1.5.5"]
+                       [org.clojure/tools.nrepl "0.2.7"]]
         :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [refactor-nrepl "0.2.2"]
                   [lein-try "0.4.3"]
