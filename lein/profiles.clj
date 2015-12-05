e5969000c11cec7781174efd368d4f8cf79c1cf8
Ivan Kryvoruchko <gildraug@gmail.com>
2015-12-05 12:15:25 +0200

    Update dependencies

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [criterium "0.4.3"]]
-        :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+        :plugins [[cider/cider-nrepl "0.10.0"]
                   [refactor-nrepl "2.0.0-SNAPSHOT"]
                   [lein-ancient "0.6.8"]
                   [com.jakemccrary/lein-test-refresh "0.11.0"]]}}
