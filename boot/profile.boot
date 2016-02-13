f230e47883a4f2bb6819b30ce6d78a1646f0c94d
Ivan Kryvoruchko <gildraug@gmail.com>
2016-02-13 12:40:38 +0200

    Update dependencies

--- boot/profile.boot
+++ boot/profile.boot
@@ -3,7 +3,7 @@
 (swap! boot.repl/*default-dependencies*
        concat '[[cider/cider-nrepl "0.11.0-SNAPSHOT"]
                 [org.clojure/tools.nrepl "0.2.12"]
-                [refactor-nrepl "2.1.0-alpha1"]
+                [refactor-nrepl "2.2.0-SNAPSHOT"]
                 [criterium "0.4.3"]])
 
 (swap! boot.repl/*default-middleware*
--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [criterium "0.4.3"]]
         :plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
-                  [refactor-nrepl "2.1.0-alpha1"]
+                  [refactor-nrepl "2.2.0-SNAPSHOT"]
                   [lein-ancient "0.6.8"]
-                  [com.jakemccrary/lein-test-refresh "0.12.0"]]}}
+                  [com.jakemccrary/lein-test-refresh "0.13.0"]]}}
