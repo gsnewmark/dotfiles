a9526c9443c272990cd56c16494da5a5e0e3ac9d
Ivan Kryvoruchko <gildraug@gmail.com>
2016-09-03 11:45:44 +0300

    Update dependencies

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,4 +2,4 @@
         :plugins [[refactor-nrepl "2.3.0-SNAPSHOT"]
                   [cider/cider-nrepl "0.14.0-SNAPSHOT"]
                   [lein-ancient "0.6.10"]
-                  [com.jakemccrary/lein-test-refresh "0.16.0"]]}}
+                  [com.jakemccrary/lein-test-refresh "0.17.0"]]}}
