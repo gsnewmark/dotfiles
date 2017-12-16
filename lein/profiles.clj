4772c134c3704ef1fd85f4da91b4d7a1a0ffda37
Ivan Kryvoruchko <gildraug@gmail.com>
2017-12-16 11:55:37 +0200

    Update dependencies

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,3 @@
 {:user {:dependencies [[criterium "0.4.4"]]
-        :plugins [[lein-ancient "0.6.14"]
-                  [com.jakemccrary/lein-test-refresh "0.21.1"]]}}
+        :plugins [[lein-ancient "0.6.15"]
+                  [com.jakemccrary/lein-test-refresh "0.22.0"]]}}
