1198dc381c83c243ef6813972c6ceaa252dc364f
Ivan Kryvoruchko <gildraug@gmail.com>
2018-03-14 11:42:00 +0200

    Add clj-memory-meter to Clojure REPL user dependencies

--- boot/profile.boot
+++ boot/profile.boot
@@ -1,4 +1,5 @@
 (require 'boot.repl)
 
 (swap! boot.repl/*default-dependencies*
-       concat '[[criterium "0.4.4"]])
+       concat '[[criterium "0.4.4"]
+                [com.clojure-goes-fast/clj-memory-meter "0.1.0"]])
--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,4 @@
-{:user {:dependencies [[criterium "0.4.4"]]
+{:user {:dependencies [[criterium "0.4.4"]
+                       [com.clojure-goes-fast/clj-memory-meter "0.1.0"]]
         :plugins [[lein-ancient "0.6.15"]
                   [com.jakemccrary/lein-test-refresh "0.22.0"]]}}
