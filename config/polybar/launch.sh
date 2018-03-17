8f1e578830b00707a4cf85a17d05b0283f0a4e1d
Ivan Kryvoruchko <gildraug@gmail.com>
2018-03-17 15:04:51 +0200

    Do not auto reload polybar config

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -8,7 +8,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload top &
+        MONITOR=$m polybar top &
     done
 else
     polybar --reload example &
