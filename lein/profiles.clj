d7138ab6258ed314082fc213d255c5e90d365eb7
Ivan Kryvoruchko <gildraug@gmail.com>
2018-03-28 16:42:44 +0300

    Add async profiler & Java decompiler to dev Clojure deps

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,6 @@
 {:user {:dependencies [[criterium "0.4.4"]
-                       [com.clojure-goes-fast/clj-memory-meter "0.1.0"]]
+                       [com.clojure-goes-fast/clj-memory-meter "0.1.0"]
+                       [com.clojure-goes-fast/clj-async-profiler "0.1.2"]
+                       [com.clojure-goes-fast/clj-java-decompiler "0.1.0"]]
         :plugins [[lein-ancient "0.6.15"]
                   [com.jakemccrary/lein-test-refresh "0.22.0"]]}}
