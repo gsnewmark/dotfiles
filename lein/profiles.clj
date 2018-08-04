f8092bfe589e09f6c494f6c279e13e624f0cddac
Ivan Kryvoruchko <gildraug@gmail.com>
2018-08-04 10:36:42 +0300

    Update dependencies

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:dependencies [[criterium "0.4.4"]
                        [com.clojure-goes-fast/clj-memory-meter "0.1.1"]
                        [com.clojure-goes-fast/clj-async-profiler "0.1.3"]
-                       [com.clojure-goes-fast/clj-java-decompiler "0.1.0"]]
+                       [com.clojure-goes-fast/clj-java-decompiler "0.1.1"]]
         :plugins [[lein-ancient "0.6.15"]
                   [com.jakemccrary/lein-test-refresh "0.23.0"]]}}
