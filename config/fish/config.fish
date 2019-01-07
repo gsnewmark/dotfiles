f31c6054d6a68f8a238f8dad22882b99d24f5d97
Ivan Kryvoruchko <gildraug@gmail.com>
2019-01-07 13:10:20 +0200

    Add support for GTK themes in Qt apps
    It also include a workaround for Calibre specifically.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,9 @@
 
 set -gx TERM xterm-256color
 
+set -gx QT_QPA_PLATFORMTHEME gtk2
+set -gx CALIBRE_USE_SYSTEM_THEME 1
+
 set -gx PATH ~/bin ~/.cargo/bin (ruby -e 'puts Gem.user_dir')/bin ~/.emacs.d/bin $PATH
 
 set -l TE "emacs -nw"
