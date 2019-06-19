50c99aa5a02a907a7931275d134bb58cae45f3a4
Ivan Kryvoruchko <gildraug@gmail.com>
2019-06-19 12:43:40 +0300

    Enable pyenv support in Doom Emacs

--- doom.d/init.el
+++ doom.d/init.el
@@ -142,7 +142,8 @@
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        (python           ; beautiful is better than ugly
-        +conda)
+        +conda
+        +pyenv)
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;rest              ; Emacs as a REST client
