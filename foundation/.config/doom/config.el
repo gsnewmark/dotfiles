a2e28848d54467783c8455e11607b60e00270cba
Ivan Kryvoruchko <gsnewmark@pm.me>
2019-11-22 21:45:58 +0200

    Switch to the M+ 1mn font

--- foundation/.config/doom/config.el
+++ foundation/.config/doom/config.el
@@ -6,10 +6,10 @@
 
 (setq doom-theme 'doom-nord)
 
-(setq doom-font (font-spec :family "Fantasque Sans Mono" :size 12.0)
+(setq doom-font (font-spec :family "M+ 1mn" :size 11.0)
       doom-variable-pitch-font (font-spec :family "Source Sans Pro")
       doom-unicode-font (font-spec :family "Source Sans Pro")
-      doom-big-font (font-spec :family "Fantasque Sans Mono" :size 18.0))
+      doom-big-font (font-spec :family "M+ 1mn" :size 17.0))
 
 (setq which-key-idle-delay 0.4)
 
