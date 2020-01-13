b6e3188123721da5882b9e3f6fab5a8ac03165f7
Ivan Kryvoruchko <gsnewmark@pm.me>
2020-01-13 12:04:03 +0200

    Switch to 'heavy' Doom Emacs modeline
    The light one somehow breaks Magit when Emacs is open for too long

--- foundation/.config/doom/init.el
+++ foundation/.config/doom/init.el
@@ -13,7 +13,8 @@
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
        (modeline         ; snazzy, Atom-inspired modeline, plus API
-        +light)
+        ;+light
+        )
        nav-flash         ; blink the current line after jumping
        ophints           ; highlight the region an operation acts on
        (popup            ; tame sudden yet inevitable temporary windows
