dade355692ba2b3ca97e9addc37b01d2e4710910
Ivan Kryvoruchko <gsnewmark@pm.me>
2020-01-24 17:24:01 +0200

    Enable memtest for NixOS

--- default.nix
+++ default.nix
@@ -30,6 +30,7 @@
 
   boot.cleanTmpDir = true;
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.memtest86.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   environment = {
