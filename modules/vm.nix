34194a961fad4cfc6001273cc428a9698f475e21
Ivan Kryvoruchko <gsnewmark@pm.me>
2020-02-13 19:48:46 +0200

    Remove VirtualBox

--- modules/vm.nix
+++ modules/vm.nix
@@ -1,15 +1,10 @@
 { config, lib, pkgs, ... }:
 
 {
-  virtualisation.virtualbox.host = {
-    enable = true;
-    enableExtensionPack = true;
-  };
-
   virtualisation.docker.enable = true;
   environment.systemPackages = with pkgs; [
     docker-compose
   ];
 
-  users.users.gsnewmark.extraGroups = [ "docker" "vboxusers" ];
+  users.users.gsnewmark.extraGroups = [ "docker" ];
 }
