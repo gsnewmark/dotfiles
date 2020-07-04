b250f3b10835a891fdb30e274393ec2a5ec97de0
Ivan Kryvoruchko <gsnewmark@pm.me>
2020-07-04 12:52:57 +0300

    Update criterium (Clojure benchmarking lib)

--- config/dev/.lein/profiles.clj
+++ config/dev/.lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:dependencies [[criterium "0.4.5"]
+{:user {:dependencies [[criterium "0.4.6"]
                        [com.clojure-goes-fast/clj-memory-meter "0.1.2"]
                        [com.clojure-goes-fast/clj-async-profiler "0.4.1"]
                        [com.clojure-goes-fast/clj-java-decompiler "0.3.0"]
