1e5741b541ec2edb88cd8b0fc009c13f447dfbfc
Ivan Kryvoruchko <gsnewmark@pm.me>
2020-08-15 12:58:51 +0300

    Update clj-memory-meter

--- config/dev/.lein/profiles.clj
+++ config/dev/.lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:dependencies [[criterium "0.4.6"]
-                       [com.clojure-goes-fast/clj-memory-meter "0.1.2"]
+                       [com.clojure-goes-fast/clj-memory-meter "0.1.3"]
                        [com.clojure-goes-fast/clj-async-profiler "0.4.1"]
                        [com.clojure-goes-fast/clj-java-decompiler "0.3.0"]
                        [com.hypirion/clj-xchart "0.2.0"]]
