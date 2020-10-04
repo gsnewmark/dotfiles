39acb1d094d2cd0a5310694957bdc98956fa4165
Ivan Kryvoruchko <gsnewmark@pm.me>
2020-10-04 12:50:25 +0300

    Disable soft wrapping in Emacs

--- config/emacs/.config/doom/init.el
+++ config/emacs/.config/doom/init.el
@@ -38,7 +38,7 @@
        multiple-cursors  ; editing in many places at once
        rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
-       word-wrap         ; soft wrapping with language-aware indent
+       ;; word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
        (dired            ; making dired pretty [functional]
