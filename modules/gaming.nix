07d9e578533eca60e09d1736088310a10281a59c
Ivan Kryvoruchko <gsnewmark@pm.me>
2020-10-08 22:13:43 +0300

    Install protontricks

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -1,7 +1,11 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ unstable.lutris steam ];
+  environment.systemPackages = with pkgs; [
+    unstable.lutris
+    protontricks
+    steam
+  ];
 
   hardware.opengl.driSupport32Bit = true;
   hardware.pulseaudio.support32Bit = true;
