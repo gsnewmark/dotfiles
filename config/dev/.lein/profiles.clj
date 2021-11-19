ca8b4cb122c2c0a5ff8ed8f7f03c4c8c19d9c024
Ivan Kryvoruchko <gsnewmark@pm.me>
2021-11-19 11:44:22 +0200

    Update common Clojure deps

--- config/dev/.lein/profiles.clj
+++ config/dev/.lein/profiles.clj
@@ -1,10 +1,10 @@
 {:user {:dependencies [[criterium "0.4.6"]
                        [com.clojure-goes-fast/clj-memory-meter "0.1.3"]
                        [com.clojure-goes-fast/clj-async-profiler "0.5.1"]
-                       [com.clojure-goes-fast/clj-java-decompiler "0.3.0"]
+                       [com.clojure-goes-fast/clj-java-decompiler "0.3.1"]
                        [com.hypirion/clj-xchart "0.2.0"]]
         :plugins [[lein-ancient "0.7.0"]
                   [com.jakemccrary/lein-test-refresh "0.25.0"]
-                  [s3-wagon-private "1.3.4"]]
+                  [s3-wagon-private "1.3.5"]]
         :plugin-repositories [["attendify" {:url           "s3p://attendify-clojars/releases/"
                                             :sign-releases false}]]}}
