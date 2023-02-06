5811e4598e8b990bce167000c90a96c1d014930d
Ivan Kryvoruchko <gsnewmark@pm.me>
2023-02-06 11:11:14 +0200

    Set Catppuccin theme for the FZF

--- config/shell/.config/fish/config.fish
+++ config/shell/.config/fish/config.fish
@@ -52,7 +52,10 @@ end
 
 ## fzf
 
-set -gx FZF_DEFAULT_OPTS '--color fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C --color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B'
+set -Ux FZF_DEFAULT_OPTS "\
+--color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 \
+--color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf \
+--color=marker:#f2d5cf,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284"
 
 ## direnv integration
 
