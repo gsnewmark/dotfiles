eeaac4d24dcfb12e7945b33b7deedb04cfac32d8
Ivan Kryvoruchko <gsnewmark@pm.me>
2024-01-16 13:35:37 +0200

    Start WezTerm in fullscreen

--- config/shell/.config/wezterm/wezterm.lua
+++ config/shell/.config/wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local mux = wezterm.mux
 
 -- This table will hold the configuration.
 local config = {}
@@ -12,9 +13,25 @@ end
 
 -- This is where you actually apply your config choices
 config.color_scheme = 'Gruvbox Material (Gogh)'
+
 config.font = wezterm.font('Iosevka Nerd Font', { weight = 'Bold' })
 config.font_size = 13.0
 config.line_height = 0.8
+
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 10
+
+config.native_macos_fullscreen_mode = true
+
+wezterm.on('gui-attached', function(domain)
+  -- maximize all displayed windows on startup
+  local workspace = mux.get_active_workspace()
+  for _, window in ipairs(mux.all_windows()) do
+    if window:get_workspace() == workspace then
+      window:gui_window():maximize()
+    end
+  end
+end)
 
 -- and finally, return the configuration to wezterm
 return config
