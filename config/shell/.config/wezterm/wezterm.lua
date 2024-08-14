50c9b8073c78140957e2161ef9ab55ca2f923210
Ivan Kryvoruchko <gsnewmark@pm.me>
2024-08-14 15:18:06 +0300

    chore: adjust Wez window decorations

--- config/shell/.config/wezterm/wezterm.lua
+++ config/shell/.config/wezterm/wezterm.lua
@@ -8,7 +8,7 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
@@ -20,17 +20,18 @@ config.line_height = 0.8
 
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 10
+config.window_decorations = 'RESIZE|INTEGRATED_BUTTONS'
 
 config.native_macos_fullscreen_mode = true
 
 wezterm.on('gui-attached', function(domain)
-  -- maximize all displayed windows on startup
-  local workspace = mux.get_active_workspace()
-  for _, window in ipairs(mux.all_windows()) do
-    if window:get_workspace() == workspace then
-      window:gui_window():maximize()
+    -- maximize all displayed windows on startup
+    local workspace = mux.get_active_workspace()
+    for _, window in ipairs(mux.all_windows()) do
+        if window:get_workspace() == workspace then
+            window:gui_window():maximize()
+        end
     end
-  end
 end)
 
 -- and finally, return the configuration to wezterm
